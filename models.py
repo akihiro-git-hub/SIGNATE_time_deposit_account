2c37baac90f35fc7a57f6fcb840c0c71b4514596
akihiro-git-hub <odoru3657x@outlook.jp>
2020-09-01 07:17:50 +0900

    コンペティション修了。最終スコアはAUC：0.8487949。

--- models.py
+++ models.py
@@ -26,6 +26,8 @@ from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
 from keras.layers.normalization import BatchNormalization
 
+
+
 class Model1xgb:
 
   def __init__(self):
@@ -55,6 +57,7 @@ class Model1xgb:
     return pred
 
 
+
 class Model1xgb2:
 
   def __init__(self):
@@ -67,10 +70,10 @@ class Model1xgb2:
           'silent':1,
           'random_state':71,
           'eta': 0.1,
-          'alpha': 0.0003595462667444797,'colsample_bytree': 0.8,
-          'gamma': 0.03280399314969842,'lambda': 0.35199860793869975,
-         'max_depth': 7,'min_child_weight': 5.189327191467295,
-         'subsample': 0.8500000000000001}
+          'alpha': 1.3070089763450425e-07,'colsample_bytree': 0.65,
+          'gamma':  0.3608243826998714,'lambda': 0.48268839914164746,
+         'max_depth': 4,'min_child_weight':3.1854414656012136,
+         'subsample': 0.75}
     num_round = 500
     self.scaler = StandardScaler()
     self.scaler.fit(tr_x)
@@ -86,6 +89,8 @@ class Model1xgb2:
     data = xgb.DMatrix(x)
     pred = self.model.predict(data)
     return pred
+
+
 
 
 class Model1xgb3:
@@ -117,7 +122,7 @@ class Model1xgb3:
   def predict(self,x):
     x = self.scaler.transform(x)
     data = xgb.DMatrix(x)
-    pred = self.model.predict(data)
+    pred = self.model.predict(data, ntree_limit=self.model.best_ntree_limit)
     return pred
 
 
@@ -168,21 +173,29 @@ class Model1NN2proba:
   def fit(self, tr_x, tr_y, va_x, va_y):
     self.scaler = StandardScaler()
     self.scaler.fit(tr_x)
-    batch_size = 128
+    batch_size = 96
     epochs = 300
     tr_x = self.scaler.transform(tr_x)
     va_x = self.scaler.transform(va_x)
     model = Sequential()
     model.add(Dropout(0.05, input_shape=(tr_x.shape[1],)))
-    model.add(Dense(96,activation=PReLU()))
-    model.add(Dropout(0.1))
-    model.add(Dense(96,activation=PReLU()))
-    model.add(Dropout(0.1))
-    model.add(Dense(96,activation=PReLU()))
-    model.add(Dropout(0.1))
+
+    model.add(Dense(32))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.25))
+    model.add(Dense(32))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.25))
+    model.add(Dense(32))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.25))
+
     model.add(Dense(1,activation = 'sigmoid'))
     model.compile(loss = 'binary_crossentropy',
-             optimizer = SGD(lr=0.0016590360920686548, decay=1e-6, momentum=0.9, nesterov=True),
+             optimizer = Adam(lr=0.008155515901825835,beta_1=0.9, beta_2=0.999, decay=0.),
               metrics=['accuracy'])
     early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
     history = model.fit(tr_x,tr_y,
@@ -195,42 +208,6 @@ class Model1NN2proba:
     x = self.scaler.transform(x)
     y_pred = self.model.predict_proba(x).reshape(-1)
     return y_pred
-
-
-class Model1NN3proba:
-  def __init__(self):
-    self.model = None
-    self.scaler =None
-  
-  def fit(self, tr_x, tr_y, va_x, va_y):
-    self.scaler = StandardScaler()
-    self.scaler.fit(tr_x)
-    batch_size = 32
-    epochs = 300
-    tr_x = self.scaler.transform(tr_x)
-    va_x = self.scaler.transform(va_x)
-    model = Sequential()
-    model.add(Dropout(0.1, input_shape=(tr_x.shape[1],)))
-    model.add(Dense(64,activation='relu'))
-    model.add(Dropout(0.05))
-    model.add(Dense(64,activation='relu'))
-    model.add(Dropout(0.05))
-    model.add(Dense(1,activation = 'sigmoid'))
-    model.compile(loss = 'binary_crossentropy',
-             optimizer = Adam(lr=5.417198646313062e-05, beta_1=0.9, beta_2=0.999, decay=0),
-              metrics=['accuracy'])
-    early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
-    history = model.fit(tr_x,tr_y,
-    batch_size=batch_size, epochs=epochs,verbose=1,validation_data = (va_x,va_y),
-    callbacks=[early_stopping])
-    self.model = model
-
-
-  def predict(self,x):
-    x = self.scaler.transform(x)
-    y_pred = self.model.predict_proba(x).reshape(-1)
-    return y_pred
-
 
 
     
@@ -252,6 +229,30 @@ class Model1ramdom:
     x = self.scaler.transform(x)
     pred = self.model.predict(x)
     return pred
+
+
+class Model1ramdom2:
+
+    def __init__(self,params=None):
+        self.params = None
+        if params is None:
+            self.params = {}
+        else :
+            self.params = params
+
+    def fit(self, tr_x, tr_y, va_x, va_y):
+        self.scaler = StandardScaler()
+        self.scaler.fit(tr_x)
+        tr_x = self.scaler.transform(tr_x)
+        self.model = RandomForestRegressor(n_estimators=150,max_features=0.1495154893874491,
+                                          min_samples_split=17,random_state=42)
+        self.model.fit(tr_x, tr_y)
+
+    def predict(self, x):
+        x = self.scaler.transform(x)
+        pred = self.model.predict(x)
+        return pred
+
 
 
 class Model1xgbsoftmax:
@@ -423,7 +424,7 @@ class Model2KMeans:
 
     cust_array = np.array(cust_array)
     cust_array = cust_array.T
-    self.model = KMeans(n_clusters = 2)
+    self.model = KMeans(n_clusters = 2,random_state=71)
     self.model.fit(cust_array)
     
   def predict(self,x):
@@ -439,6 +440,48 @@ class Model2KMeans:
     cust_test = cust_test.T
     pred = self.model.fit_predict(x)
     return pred
+
+
+class Model2KMeans2:
+  def __init__(self):
+    self.model = None
+    self.scaler = None
+
+  def fit(self, tr_x, tr_y, va_x, va_y):
+    self.scaler = StandardScaler()
+    self.scaler.fit(tr_x)
+    tr_x = self.scaler.transform(tr_x)
+    tr_x = pd.DataFrame(tr_x)
+
+    cust_array = []
+    for i in range (tr_x.shape[1]):
+      list_s = tr_x.iloc[:,1].tolist()
+      cust_array.append(list_s)
+
+
+    cust_array = np.array(cust_array)
+    cust_array = cust_array.T
+    self.model = KMeans(n_clusters = 2,ramdom_state=1021),
+    self.model.fit(cust_array)
+    
+  def predict(self,x):
+    x = self.scaler.transform(x)
+    x = pd.DataFrame(x)
+    cust_test = []
+    for c in range(x.shape[1]):
+      list_s = x.iloc[:,c].tolist()
+      cust_test.append(list_s)
+    
+
+    cust_test = np.array(cust_test)
+    cust_test = cust_test.T
+    pred = self.model.fit_predict(x)
+    return pred
+
+
+
+
+
 
 
 
@@ -519,6 +562,53 @@ class Model2KNN_p:
     return pred
 
 
+class Model2NNproba:
+  def __init__(self):
+    self.model = None
+    self.scaler =None
+  
+  def fit(self, tr_x, tr_y, va_x, va_y):
+    self.scaler = StandardScaler()
+    self.scaler.fit(tr_x)
+    batch_size = 96
+    epochs = 300
+    tr_x = self.scaler.transform(tr_x)
+    va_x = self.scaler.transform(va_x)
+    model = Sequential()
+    model.add(Dropout(0.15000000000000002, input_shape=(tr_x.shape[1],)))
+
+    model.add(Dense(64))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.2))
+    model.add(Dense(64))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.2))
+    model.add(Dense(64))
+    model.add(BatchNormalization())
+    model.add(PReLU())
+    model.add(Dropout(0.2))
+    
+    
+    
+    model.add(Dense(1,activation = 'sigmoid'))
+    model.compile(loss = 'binary_crossentropy',
+             optimizer = Adam(lr= 0.000360244809647964, beta_1=0.9, beta_2=0.999, decay=0.),
+              metrics=['accuracy'])
+    early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
+    history = model.fit(tr_x,tr_y,
+    batch_size=batch_size, epochs=epochs,verbose=1,validation_data = (va_x,va_y),
+    callbacks=[early_stopping])
+    self.model = model
+
+
+  def predict(self,x):
+    x = self.scaler.transform(x)
+    y_pred = self.model.predict_proba(x).reshape(-1)
+    return y_pred
+
+
 
 class Model3logistic:
 
@@ -537,6 +627,8 @@ class Model3logistic:
     x = self.scaler.transform(x)
     pred = self.model.predict(x)
     return pred
+
+
 
 class Model3NNproba:
   def __init__(self):
